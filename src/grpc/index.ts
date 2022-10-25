c14cbad6211875b8cda39214ec8f611f01fa4eca
Christoph Bühler <christoph@smartive.ch>
2022-10-25 10:24:58 +0200

    feat: export service client types from grpc file

--- src/grpc/index.ts
+++ src/grpc/index.ts
@@ -1,2 +1,5 @@
 export * from './clients';
+export { AdminServiceClient } from './generated/zitadel/admin';
+export { AuthServiceClient } from './generated/zitadel/auth';
+export { ManagementServiceClient } from './generated/zitadel/management';
 export * from './interceptors';
